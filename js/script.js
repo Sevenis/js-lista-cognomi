a0199da104e0501780e6c5e86eb28c207d4222ce
Pietro Sette <pietrosette@gmail.com>
2020-07-13 17:08:08 +0200

    Commenti inseriti

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
+// INIZIALIZZAZIONI VARIABILI
 var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
 var listaCognomiOrdinata = [];
 var cognome;
-var check=false;
+var check = false;
 
+// INPUT
 cognome = prompt('Inserisci il tuo cognome');
 
-for(i=0; i<listaCognomi.length; i++){
+// CHECK PRE ESISTENZA COGNOME NELLA LISTA
+for(var i = 0; i < listaCognomi.length; i++){
     if(cognome == listaCognomi[i]){
         check=true;
     }
 }
 
-if(check==true){
+// RISOLUZIONE CHECK (Alert OR PUSH)
+if(check == true){
     alert("Cognome già esistente!")
 } else {
     listaCognomi.push(cognome);
 }
 
-for (var j=0; j<listaCognomi.length; j++){
+// ORDINAMENTO LISTA
+for (var j = 0; j < listaCognomi.length; j++){
     listaCognomiOrdinata.push(listaCognomi[j].toLowerCase());
     listaCognomiOrdinata.sort();
 }
 
-for (var k=0; k<listaCognomiOrdinata.length; k++) {
+// STAMPA LISTA ORDINATA
+for (var k = 0; k < listaCognomiOrdinata.length; k++) {
     document.getElementById('lista').innerHTML += '<li>' + listaCognomiOrdinata[k].charAt(0).toUpperCase()+listaCognomiOrdinata[k].slice(1)+ '</li>';
 }
