c77b7dfffbb5d81f8ff3dc9efbbd26059d1b4a37
Pietro Sette <pietrosette@gmail.com>
2020-07-13 16:38:20 +0200

    Creazione Lista Ordinata done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
 var listaCognomiOrdinata = [];
 var cognome;
+var check=false;
 
-listaCognomi.push(prompt('Inserisci il tuo cognome'));
+cognome = prompt('Inserisci il tuo cognome');
+console.log(cognome);
 
-for (var i=0; i<listaCognomi.length; i++){
-    listaCognomiOrdinata.push(listaCognomi[i]);
+for(i=0; i<listaCognomi.length; i++){
+    if(cognome == listaCognomi[i]){
+        check=true;
+    }
+}
+console.log(check);
+if(check==true){
+    alert("Cognome già esistente!")
+} else {
+    listaCognomi.push(cognome);
+}
+
+for (var j=0; j<listaCognomi.length; j++){
+    listaCognomiOrdinata.push(listaCognomi[j].toLowerCase());
     listaCognomiOrdinata.sort();
 }
-console.log(listaCognomiOrdinata);
+
+for (var k=0; k<listaCognomiOrdinata.length; k++) {
+    document.getElementById('lista').innerHTML += '<li>' + listaCognomiOrdinata[k].charAt(0).toUpperCase()+listaCognomiOrdinata[k].slice(1)+ '</li>';
+}
